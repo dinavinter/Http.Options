b17921ff82e91e0483c9c10fa4314b81940c1adb
din <dina.vinter@sap.com>
2020-12-06 17:43:14 +0200

    disable timeout by default

--- Http.Client.Options/PollyOptions/TimeoutPolicyOptions.cs
+++ Http.Client.Options/PollyOptions/TimeoutPolicyOptions.cs
@@ -6,11 +6,7 @@ namespace Http.Options
 {
     //TODO use polly timeout instead of handler
     public class TimeoutPolicyOptions<T> : PolicyOptions<T>
-    {
-        public TimeoutPolicyOptions()
-        {
-            Enabled = true;
-        }
+    { 
         public int TimeoutMS { get; set; } = 2000;
 
         public IAsyncPolicy<T> Polly() => PolicyOrNoOP(Policy.TimeoutAsync<T>(timeout: TimeSpan.FromMilliseconds(TimeoutMS), TimeoutStrategy.Pessimistic));
