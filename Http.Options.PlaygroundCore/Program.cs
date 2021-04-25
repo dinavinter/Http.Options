132a4118fe3b3acdc4909e7c07bd1523a3b7d613
din <dina.vinter@sap.com>
2021-04-25 23:02:53 +0300

    playground

---
+++ Http.Options.PlaygroundCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Http.Options.PlaygroundCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
