02b2597bb5542e59b45b1a104cf78857f10b5b82
din <dina.vinter@sap.com>
2021-05-26 17:37:44 +0300

    http tags

--- Http.Client.Options/Tracing/Tracers/FlowTracer.cs
+++ Http.Client.Options/Tracing/Tracers/FlowTracer.cs
@@ -5,10 +5,15 @@ namespace Http.Options.Tracing
 {
     public class FlowTracer
     {
-        public TracingTag RequestStart = "time.start";
         public TracingTag Timestamp = "timestamp";
+        public TracingTag RequestStart = "time.start"; 
         public TracingTag RequestEnd = "time.end";
-        public TracingTag TotalTime = "time.total";
+        public TracingTag TotalTime = "time.duration";
+        
+        public TracingTag HttpRequestStart = "time.http.start";  
+        public TracingTag HttpRequestEnd = "time.http.end"; 
+        public TracingTag HttpTotalTime = "time.http.duration";
+
         public TracingTag CorrelationsId = "correlation.id";
 
         public void TraceStart(HttpTracingActivity activity)
@@ -27,6 +32,10 @@ namespace Http.Options.Tracing
 
             if (activity.HttpActivity != null)
             {
+                activity[HttpRequestStart] = activity.HttpActivity.StartTimeUtc;
+                activity[HttpRequestEnd] = activity.HttpActivity.StartTimeUtc.Add(activity.HttpActivity.Duration);
+                activity[HttpTotalTime] = activity.HttpActivity.Duration;
+
                 foreach (var httpActivityTag in activity.HttpActivity.Tags)
                 {
                     activity[httpActivityTag.Key] = httpActivityTag.Value;
