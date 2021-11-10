8dd95675ccedf091059f3ae70d63af6a43505942
dinavinter <dina.vintr@gmail.com>
2021-11-10 13:31:41 +0200

    5.0.1-preview

--- nuget_push_gigya.bat
+++ nuget_push_gigya.bat
@@ -1,2 +1,2 @@
-dotnet nuget push "**/*.4.0.14-preview.symbols.nupkg" -s https://nuget.gigya.net/nugetForBuild/nuget
-dotnet nuget push "**/*.4.0.14-preview.nupkg" -s https://nuget.gigya.net/nugetForVS/nuget
+dotnet nuget push "**/*.5.0.1-preview.symbols.nupkg" -s https://nuget.gigya.net/nugetForBuild/nuget
+dotnet nuget push "**/*.5.0.1-preview.nupkg" -s https://nuget.gigya.net/nugetForVS/nuget
